56112889f11807e4baa9e90028a0bb0434a80714
omupenguin <quek.aik.peng@gmail.com>
2020-04-22 15:12:17 +0800

    Added lots of stuff :(

---
+++ Assets/Scripts/Dialogue/NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity.BartenderOdyssey 
+{
+    public class NPC : MonoBehaviour
+    {
+        public string characterName = "";
+        public string talkToNode = "";
+
+        [Header("Optional")]
+        public YarnProgram scriptToLoad;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (scriptToLoad != null)
+            {
+                DialogueRunner dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+                dialogueRunner.Add(scriptToLoad);
+            }
+        }
+    }
+}
