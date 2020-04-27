075502f6094fea8fb7bbea19ee7e8a40044597d3
omupenguin <quek.aik.peng@gmail.com>
2020-04-27 15:39:54 +0800

    Added pouring, and filling cups

---
+++ Assets/myAssets/Scripts/PourEndpointColliderScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourEndpointColliderScript : MonoBehaviour
+{
+
+    public void OnTriggerStay(Collider other)
+    {
+        /*
+        if (other.name.Contains("DrinkCollider"))
+        {
+            other.transform.parent.gameObject.transform.Find("Drink").GetComponent<CupLiquid>().fillCup();
+        }*/
+
+        if (other.name.Contains("Drink") || other.name.Contains("DrinkCollider"))
+        {
+            other.transform.parent.gameObject.transform.Find("Drink").GetComponent<CupLiquid>().fillCup();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/myAssets/Scripts/Stream.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour
+{
+
+    public GameObject drinkEndpointCollider;
+    public LayerMask defaultLayer;
+    private GameObject endptCollider = null;
+
+    private LineRenderer lineRenderer = null;
+    private Vector3 targetPosition = Vector3.zero;
+    private ParticleSystem splashParticle = null;
+
+    private Coroutine pourRoutine = null;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        splashParticle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+    }
+
+    public void Begin()
+    {
+        endptCollider = Instantiate(drinkEndpointCollider, transform.position, Quaternion.identity, transform);
+        StartCoroutine(UpdateParticles());
+        pourRoutine = StartCoroutine(BeginPour());
+    }
+
+    private IEnumerator BeginPour()
+    {
+
+        // Run while object is active
+        while(gameObject.activeSelf)
+        {
+            targetPosition = FindEndPoint();
+
+            MoveToPosition(0, transform.position);
+            AnimateToPosition(1, targetPosition);
+
+            endptCollider.transform.position = targetPosition;
+
+            yield return null;
+        }
+    }
+
+    public void End()
+    {
+        StopCoroutine(pourRoutine);
+        pourRoutine = StartCoroutine(EndPour());
+    }
+
+    private IEnumerator EndPour()
+    {
+        while(!HasReachedPosition(0, targetPosition))
+        {
+            AnimateToPosition(0, targetPosition);
+            AnimateToPosition(1, targetPosition);
+        
+            yield return null;
+        }
+
+        Destroy(endptCollider);
+        Destroy(gameObject);
+    }
+
+
+    private Vector3 FindEndPoint()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Physics.Raycast(ray, out hit, 2.0f, defaultLayer);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(2.0f);
+
+        return endPoint;
+    }
+
+    private void MoveToPosition(int index, Vector3 targetPosition)
+    {
+        lineRenderer.SetPosition(index, targetPosition);
+    }
+
+    private void AnimateToPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPoint = lineRenderer.GetPosition(index);
+        Vector3 newPosition = Vector3.MoveTowards(currentPoint, targetPosition, Time.deltaTime * 1.75f);
+        lineRenderer.SetPosition(index, newPosition);
+    }
+
+    private bool HasReachedPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPosition = lineRenderer.GetPosition(index);
+        return currentPosition == targetPosition;
+    }
+
+    private IEnumerator UpdateParticles()
+    {
+
+        while(gameObject.activeSelf)
+        {
+            splashParticle.gameObject.transform.position = targetPosition;
+
+            bool isHitting = HasReachedPosition(1, targetPosition);
+            splashParticle.gameObject.SetActive(isHitting);
+            
+            yield return null;
+        }
+    }
+}
